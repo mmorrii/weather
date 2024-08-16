27cf93e1104ce152db1a7912e70c9dd3393f0cb0
mmorrii <byryshka55@gmail.com>
2024-08-16 22:20:59 +0300

    added tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,16 @@
+const {nextui} = require("@nextui-org/react");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: "class",
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    content: [
+        "./index.html",
+        "./src/**/*.{js,ts,jsx,tsx}",
+        "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
+    ],
+    theme: {
+        extend: {},
+    },
+    darkMode: "class",
+    plugins: [nextui()],
 }
 
