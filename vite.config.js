813045d405e6669cf7cec6183101b88ba8eb6d84
mmorrii <byryshka55@gmail.com>
2024-08-16 20:07:01 +0300

    added vite base

--- vite.config.js
+++ vite.config.js
@@ -5,10 +5,11 @@ import autoprefixer from 'autoprefixer'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  css: {
-    postcss: {
-      plugins: [tailwindcss, autoprefixer],
+    plugins: [react()],
+    base: "weather",
+    css: {
+        postcss: {
+            plugins: [tailwindcss, autoprefixer],
+        },
     },
-  },
 })
