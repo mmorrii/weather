df30bab80c31ad810f575ea222e1b52f5046c3bb
mmorrii <byryshka55@gmail.com>
2024-08-18 00:56:09 +0300

    changed function definitions current season

--- src/utils/current-season.js
+++ src/utils/current-season.js
@@ -1,37 +1,33 @@
 import {DateTime} from "luxon";
 
 const seasons = {
-	"Зима": [12, 1, 2],
-	"Весна": [3, 4, 5],
-	"Лето": [6, 7, 8],
-	"Осень": [9, 10, 11]
-};
+	"winter": [12, 1, 2],
+	"spring": [3, 4, 5],
+	"summer": [6, 7, 8],
+	"autumn": [9, 10, 11]
+}
 
 const getCurrentMonth = (tz) => {
 	const month = DateTime.local().setZone(tz).c.month;
-	return Object.keys(seasons).find(season => seasons[season].includes(month));
-};
-
-const isEquatorCountries = (lat) => {
-	return Math.abs(lat) <= 25
+	return Object.keys(seasons).find(season => seasons[season].includes(month))
 }
 
 const isSnowCode = (code) => {
-	return code === 71 || code === 73 || code === 75 || code === 77;
+	return code === 71 || code === 73 || code === 75 || code === 77
 }
 
 export const currentSeason = (code, tz, lat) => {
 	const month = getCurrentMonth(tz)
 	const isSnow = isSnowCode(code)
-	const isEquator = isEquatorCountries(lat)
+	const isEquatorial = Math.abs(lat) <= 25
 
-	if ( isEquator || month === "Лето" ) {
+	if ( isEquatorial || month === "summer" ) {
 		return "summer"
-	} else if ( isSnow || month === "Зима" ) {
+	} else if ( isSnow || month === "winter" ) {
 		return "winter"
-	} else if ( month === "Весна" ) {
+	} else if ( month === "spring" ) {
 		return "spring"
-	} else if ( month === "Осень" ) {
+	} else if ( month === "autumn" ) {
 		return "autumn"
 	}
 }
