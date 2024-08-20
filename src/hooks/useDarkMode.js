cabfe6d30301dbe73cffde99a40ca830c0de1471
mmorrii <byryshka55@gmail.com>
2024-08-20 14:12:23 +0300

    changed dark mode

--- src/hooks/useDarkMode.js
+++ src/hooks/useDarkMode.js
@@ -2,24 +2,25 @@ import {useEffect, useState} from "react";
 import { useLocalStorage } from "@uidotdev/usehooks";
 
 export const useDarkMode = () => {
-	const [option, setOption] = useState("device")
-	const [darkMode, setDarkMode] = useLocalStorage("darkMode", false)
+	const [darkMode, setDarkMode] = useLocalStorage("darkMode", true)
 	
 	useEffect(() => {
-		if (option === "dark" || (option === "device" && window.matchMedia("(prefers-color-scheme: dark)").matches)) {
+		if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
 			setDarkMode(true)
 		} else {
 			setDarkMode(false)
 		}
-	}, [option])
+	}, [])
 	
 	useEffect(() => {
 		if (darkMode) {
+			document.documentElement.classList.add("dark")
 			document.body.classList.add("dark")
 		} else {
+			document.documentElement.classList.remove("dark")
 			document.body.classList.remove("dark")
 		}
 	}, [darkMode])
 
-	return {darkMode, setDarkMode, option, setOption}
+	return {darkMode, setDarkMode}
 }
