d5ae528c15a5808c182d65ee3ebfae21293cca27
mmorrii <byryshka55@gmail.com>
2024-08-21 22:53:24 +0300

    added style in index.html

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -3,7 +3,7 @@ import {Divider} from "@nextui-org/react";
 
 const Layout = ({ children }) => {
     return (
-        <div className="w-screen h-screen dark:bg-zinc-900 bg-neutral-100 dark:text-zinc-100 duration-200">
+        <div className="w-screen h-screen dark:bg-zinc-900 bg-neutral-100 duration-200">
             <Header />
             <Divider />
             <main >
