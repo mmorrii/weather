d5b954498033bf26ad6695cd410a63ec912bf42e
mmorrii <byryshka55@gmail.com>
2024-08-24 22:07:05 +0300

    added header navigation

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -1,48 +1,71 @@
 import { MapPin, Sun, Moon } from 'lucide-react';
-import {Switch} from "@nextui-org/react";
+import {Divider, Switch} from "@nextui-org/react";
 import {useDarkMode} from "../hooks/useDarkMode.js";
 import Logo from "../assets/Logo.jsx";
 import SearchField from "../components/SearchField.jsx";
-import {useLocation} from "../hooks/useLocation.js";
+import {useLocality} from "../hooks/useLocality.js";
+import {NavLink} from "react-router-dom";
 
-const Header = () => {
+export const Header = () => {
     const {darkMode, setDarkMode} = useDarkMode()
-    const {location} = useLocation()
+    const {location} = useLocality()
 
     return (
-        <header className="py-[2px] max-w-7xl w-full m-auto">
-            <div className="grid grid-cols-[34%_1fr_34%] items-center gap-[30px]">
-                <div className="flex items-center gap-[30px]">
-                    <div className="flex-[0_0_auto] w-[3.2rem]">
-                        <Logo/>
+        <>
+            <header className="py-[2px] fixedWidth">
+                <div className="grid grid-cols-[34%_1fr_34%] items-center gap-[30px]">
+                    <div className="flex items-center gap-[30px]">
+                        <div className="flex-[0_0_auto] w-[3.2rem]">
+                            <Logo/>
+                        </div>
+
+                        <div className="flex items-center gap-[4px] overflow-hidden">
+                            <div><MapPin size="1.33rem" strokeWidth="1.5"/></div>
+                            <p className="truncate">{location?.name || "N/A"}</p>
+                        </div>
                     </div>
 
-                    <div className="flex items-center gap-[4px] overflow-hidden">
-                        <div><MapPin size="1.33rem" strokeWidth="1.5"/></div>
-                        <p className="truncate">{location?.name || "N/A"}</p>
+                    <div className="justify-self-center w-full">
+                        <SearchField/>
                     </div>
-                </div>
 
-                <div className="justify-self-center w-full">
-                    <SearchField/>
+                    <Switch
+                        className="justify-self-end"
+                        defaultSelected size="sm" color="default"
+                        thumbIcon={({isSelected, className}) =>
+                            isSelected ? (
+                                <Sun className={className}/>
+                            ) : (
+                                <Moon className={className}/>
+                            )
+                        }
+                        classNames={{}}
+                        isSelected={darkMode} onValueChange={setDarkMode}
+                    />
                 </div>
+            </header>
+            <Divider className="mb-[30px]" />
+            <nav className={`flex gap-[20px] fixedWidth dark:text-zinc-200/40 text-lg`}>
+                <NavLink to="/" state={{timeStamp: 1}}
+                     className={({isActive}) => isActive ? "dark:text-zinc-200" : "hover:dark:text-zinc-200/70 duration-200"}>
+                    Today
+                </NavLink>
 
-                <Switch
-                    className="justify-self-end"
-                    defaultSelected size="sm" color="default"
-                    thumbIcon={({isSelected, className}) =>
-                        isSelected ? (
-                            <Sun className={className}/>
-                        ) : (
-                            <Moon className={className}/>
-                        )
-                    }
-                    classNames={{}}
-                    isSelected={darkMode} onValueChange={setDarkMode}
-                />
-            </div>
-        </header>
+                <NavLink to="/tomorrow" state={{timeStamp: 3}}
+                     className={({isActive}) => isActive ? "dark:text-zinc-200" : "hover:dark:text-zinc-200/70 duration-200"}>
+                    Tomorrow
+                </NavLink>
+
+                <NavLink to="/week" state={{timeStamp: 7}}
+                     className={({isActive}) => isActive ? "dark:text-zinc-200" : "hover:dark:text-zinc-200/70 duration-200"}>
+                    Next 7 days
+                </NavLink>
+
+                <NavLink to="/two-week" state={{timeStamp: 14}}
+                     className={({isActive}) => isActive ? "dark:text-zinc-200" : "hover:dark:text-zinc-200/70 duration-200"}>
+                    Next 14 days
+                </NavLink>
+            </nav>
+        </>
     )
 }
-
-export default Header
