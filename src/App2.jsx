900a85df7682ce8239da369b1b63163bee19e567
mmorrii <byryshka55@gmail.com>
2024-08-25 23:11:31 +0300

    added data router

--- src/App2.jsx
+++ src/App2.jsx
@@ -1,21 +1,45 @@
-import {Route, Routes} from "react-router-dom";
+import {createBrowserRouter, Route, Routes} from "react-router-dom";
 import {Layout} from "./layout/Layout.jsx";
 import {Home2} from "./pages/Home2.jsx";
 import {Tomorrow} from "./pages/Tomorrow.jsx";
 import {Week} from "./pages/Week.jsx";
 import {TwoWeek} from "./pages/TwoWeek.jsx";
 
-const App2 = () => {
+export const App2 = () => {
     return (
         <Routes>
             <Route path="/" element={<Layout />}>
                 <Route path="/" element={<Home2 />} />
                 <Route path="/tomorrow" element={<Tomorrow />} />
-                <Route path="/week" element={<Week />} />
+                <Route path="/week" element={<Week />} loader={() => fetch(7)} />
                 <Route path="/two-week" element={<TwoWeek />} />
             </Route>
         </Routes>
     )
 }
 
-export default App2
+export const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Layout />,
+        children: [
+            {
+                index: true,
+                element: <Home2 />,
+            },
+            {
+                path: "tomorrow",
+                element: <Tomorrow />,
+            },
+            {
+                path: "week",
+                element: <Week />,
+                // loader: () => fetch(7),
+            },
+            {
+                path: "two-week",
+                element: <TwoWeek />,
+            },
+        ],
+    }
+], { basename: "/weather" })
--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,11 @@
 import {createRoot} from 'react-dom/client';
 import './index.css';
-import {BrowserRouter} from "react-router-dom";
-import App2 from "./App2.jsx";
+import {BrowserRouter, RouterProvider} from "react-router-dom";
+import {App2, router} from "./App2.jsx";
 
 createRoot(document.getElementById('root')).render(
-    <BrowserRouter basename="weather">
-        <App2 />
-    </BrowserRouter>
+    // <BrowserRouter basename="weather">
+    //     <App2 />
+    // </BrowserRouter>
+    <RouterProvider router={router} />
 )
