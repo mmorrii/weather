9cf7a950f64520fa82dd791bae24089f2a8c0354
mmorrii <byryshka55@gmail.com>
2024-08-27 13:29:07 +0300

    added suspense to layout

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -2,18 +2,21 @@ import {Header} from "./Header.jsx";
 import {LocationProvider} from "../components/LocationProvider.jsx";
 import {ForecastProvider} from "../components/ForecastProvider.jsx";
 import {Outlet} from "react-router-dom";
+import {Suspense} from "react";
 
 export const Layout = () => {
     return (
         <LocationProvider>
-                <div className="w-screen h-screen dark:bg-zinc-900 bg-neutral-100 duration-200">
-                    <Header/>
-                    <main className="fixedWidth">
+            <div className="w-screen h-screen dark:bg-zinc-900 bg-neutral-100 duration-200">
+                <Header/>
+                <main className="fixedWidth">
+                    <Suspense fallback={<div>Loading...</div>}>
                         <ForecastProvider>
                             <Outlet/>
                         </ForecastProvider>
-                    </main>
-                </div>
+                    </Suspense>
+                </main>
+            </div>
         </LocationProvider>
     )
 }
