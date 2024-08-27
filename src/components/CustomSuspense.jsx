c19a0dd332c998fd7a9227c8a3ad892049fd2655
mmorrii <byryshka55@gmail.com>
2024-08-27 14:21:06 +0300

    added custom suspense

---
+++ src/components/CustomSuspense.jsx
@@ -0,0 +1,17 @@
+import {useForecast} from "../hooks/useForecast.js";
+import {useLocation} from "react-router-dom";
+import {Suspense} from "react";
+import {CircularProgress} from "@nextui-org/react";
+
+export const CustomSuspense = ({ children }) => {
+    const {forecast, status} = useForecast()
+    const {state} = useLocation()
+
+    return (
+        <Suspense fallback={<CircularProgress label="Loading..." className="m-auto mt-[30px]" />}>
+            { (status !== "success" && !forecast?.daily?.weather_code[state.timeStamp - 1]) ?
+                <CircularProgress label="Loading..." className="m-auto mt-[30px]" /> : children
+            }
+        </Suspense>
+    )
+}
--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -2,7 +2,7 @@ import {Header} from "./Header.jsx";
 import {LocationProvider} from "../components/LocationProvider.jsx";
 import {ForecastProvider} from "../components/ForecastProvider.jsx";
 import {Outlet} from "react-router-dom";
-import {Suspense} from "react";
+import {CustomSuspense} from "../components/CustomSuspense.jsx";
 
 export const Layout = () => {
     return (
@@ -10,11 +10,11 @@ export const Layout = () => {
             <div className="w-screen h-screen dark:bg-zinc-900 bg-neutral-100 duration-200">
                 <Header/>
                 <main className="fixedWidth">
-                    <Suspense fallback={<div>Loading...</div>}>
-                        <ForecastProvider>
+                    <ForecastProvider>
+                        <CustomSuspense>
                             <Outlet/>
-                        </ForecastProvider>
-                    </Suspense>
+                        </CustomSuspense>
+                    </ForecastProvider>
                 </main>
             </div>
         </LocationProvider>
