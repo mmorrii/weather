a6bcd26937b224bd4f5068b75d48c5e405693b96
mmorrii <byryshka55@gmail.com>
2024-08-28 22:21:53 +0300

    icons moved to public

--- src/components/WeatherIcon.jsx
+++ src/components/WeatherIcon.jsx
@@ -2,28 +2,28 @@ export const WeatherIcon = ({code, isDay = false}) => {
     switch (code) {
         case 0:
             return <div className="size-full">
-                {isDay ? <img src="/weather/src/assets/clear-sky-day.svg" title="Ясно" alt="Ясно"/> :
-                    <img src="/weather/src/assets/clear-sky-night.svg" title="Ясно" alt="Ясно"/>
+                {isDay ? <img src="/clear-sky-day.svg" title="Ясно" alt="Ясно"/> :
+                    <img src="/clear-sky-night.svg" title="Ясно" alt="Ясно"/>
                 }
             </div>
 
         case 1:
         case 2:
             return <div className="size-full">
-                {isDay ? <img src="/weather/src/assets/cloudy-day.svg" title="Облачно" alt="Облачно"/> :
-                    <img src="/weather/src/assets/cloudy-night.svg" title="Облачно" alt="Облачно"/>
+                {isDay ? <img src="/cloudy-day.svg" title="Облачно" alt="Облачно"/> :
+                    <img src="/cloudy-night.svg" title="Облачно" alt="Облачно"/>
                 }
             </div>
 
         case 3:
             return <div className="size-full">
-                <img src="/weather/src/assets/overcast.svg" title="Пасмурно" alt="Пасмурно"/>
+                <img src="/overcast.svg" title="Пасмурно" alt="Пасмурно"/>
             </div>
 
         case 45:
         case 48:
             return <div className="size-full">
-                <img src="/weather/src/assets/fog.svg" title="Туман" alt="Туман"/>
+                <img src="/fog.svg" title="Туман" alt="Туман"/>
             </div>
 
         case 51:
@@ -32,7 +32,7 @@ export const WeatherIcon = ({code, isDay = false}) => {
         case 56:
         case 57:
             return <div className="size-full">
-                <img src="/weather/src/assets/drizzle.svg" title="Морось" alt="Морось"/>
+                <img src="/drizzle.svg" title="Морось" alt="Морось"/>
             </div>
 
         case 61:
@@ -41,7 +41,7 @@ export const WeatherIcon = ({code, isDay = false}) => {
         case 66:
         case 67:
             return <div className="size-full">
-                <img src="/weather/src/assets/rain.svg" title="Дождь" alt="Дождь"/>
+                <img src="/rain.svg" title="Дождь" alt="Дождь"/>
             </div>
 
         case 71:
@@ -49,31 +49,31 @@ export const WeatherIcon = ({code, isDay = false}) => {
         case 75:
         case 77:
             return <div className="size-full">
-                <img src="/weather/src/assets/snow.svg" title="Снег" alt="Снег"/>
+                <img src="/snow.svg" title="Снег" alt="Снег"/>
             </div>
 
         case 80:
         case 81:
         case 82:
             return <div className="size-full">
-                <img src="/weather/src/assets/rain.svg" title="Ливень" alt="Ливень"/>
+                <img src="/rain.svg" title="Ливень" alt="Ливень"/>
             </div>
 
         case 85:
         case 86:
             return <div className="size-full">
-                <img src="/weather/src/assets/snow-showers.svg" title="Снег с дождем" alt="Снег с дождем"/>
+                <img src="/snow-showers.svg" title="Снег с дождем" alt="Снег с дождем"/>
             </div>
 
         case 95:
             return <div className="size-full">
-                <img src="/weather/src/assets/thunderstorm.svg" title="Гроза" alt="Гроза"/>
+                <img src="/thunderstorm.svg" title="Гроза" alt="Гроза"/>
             </div>
 
         case 96:
         case 99:
             return <div className="size-full">
-                <img src="/weather/src/assets/thunderstorm.svg" title="Гроза с градом" alt="Гроза с градом"/>
+                <img src="/thunderstorm.svg" title="Гроза с градом" alt="Гроза с градом"/>
             </div>
     }
 }
