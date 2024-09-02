05da5543c59a48cb86174c28c1aafafdaab2c3d5
mmorrii <byryshka55@gmail.com>
2024-09-02 22:57:47 +0300

    added modal to map

--- src/components/InteractiveMap.jsx
+++ src/components/InteractiveMap.jsx
@@ -1,21 +1,25 @@
 import Map, {
     FullscreenControl,
-    GeolocateControl,
+    GeolocateControl, Layer,
     Marker,
     NavigationControl,
     Popup,
-    ScaleControl, useMap,
+    ScaleControl, Source, useMap,
 } from 'react-map-gl/maplibre';
 import 'maplibre-gl/dist/maplibre-gl.css';
 import {useLocality} from "../hooks/useLocality.js";
-import {useRef} from "react";
+import {useRef, useState} from "react";
+import { Check, X } from 'lucide-react';
 
 export const InteractiveMap = () => {
     const {location} = useLocality()
     const mapRef = useRef()
 
-    const handleClick = () => {
-        console.log("i click right button")
+    const [coords, setCoords] = useState(null)
+
+    const handleClick = (event) => {
+        const { lngLat } = event
+        setCoords(lngLat)
     }
 
     return (
@@ -34,14 +38,39 @@ export const InteractiveMap = () => {
             <Marker longitude={location.longitude} latitude={location.latitude} color="red"/>
             <NavigationControl position="top-left"/>
             <ScaleControl position="bottom-left"/>
-            {/*<Popup longitude={-122.4} latitude={37.8} >*/}
-            {/*    You are here*/}
-            {/*</Popup>*/}
-            {/*<GeolocateControl />*/}
+            {coords && <Modal coords={coords} setCoords={setCoords} />}
 
-            {/*<Source id="my-data" type="geojson" data={geojson}>*/}
+            {/*<Source id="my-source" type="geojson" data={{*/}
+            {/*    type: 'FeatureCollection',*/}
+            {/*    features: [*/}
+            {/*        {type: 'Feature', geometry: {type: 'Point', coordinates: [0, 0]}}*/}
+            {/*    ]*/}
+            {/*}}>*/}
             {/*    <Layer {...layerStyle} />*/}
             {/*</Source>*/}
         </Map>
+    )
+}
+
+const Modal = ({ coords, setCoords }) => {
+    return (
+        <div
+            className="bg-neutral-800 text-[13px] rounded-full p-[3px_4px_4px_10px] absolute top-[10px] left-1/2 -translate-x-1/2 flex items-center gap-[6px]">
+            <p className="flex items-center fontSize13">Изменить местоположение на -
+                <span className="ml-[4px] max-w-24 truncate"><b className="mr-[2px] dark:text-blue-500">Ш:</b>{coords.lat}</span>,
+                <span className="ml-[4px] max-w-24 truncate"><b className="mr-[2px] dark:text-green-500">Д:</b>{coords.lng}</span>
+            </p>
+            <div className="flex items-center gap-[4px]">
+                <button type="button" className="rounded-full bg-green-600 p-1 hover:brightness-125 duration-150">
+                    <Check size={16} strokeWidth={2.5}/>
+                </button>
+                <button
+                    type="button" onClick={() => setCoords(null)}
+                    className="rounded-full bg-red-600 p-1 hover:brightness-125 duration-150"
+                >
+                    <X size={16} strokeWidth={2.5}/>
+                </button>
+            </div>
+        </div>
     )
 }
