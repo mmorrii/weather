81e39944e47e69841b2e6b50f65748da6250b76d
mmorrii <byryshka55@gmail.com>
2024-09-11 17:52:03 +0300

    changed styles in TodayForecastCard

--- src/components/TodayForecastCard.jsx
+++ src/components/TodayForecastCard.jsx
@@ -7,11 +7,9 @@ import {useForecast} from "../hooks/useForecast.js";
 export const TodayForecastCard = () => {
     const {forecast} = useForecast()
 
-    console.log(forecast)
-
     return (
-        <div className="dark:bg-zinc-600/40 rounded-lg p-[4px_10px_8px] flex flex-col gap-[16px]">
-            <p>Прогноз на {formatTime(forecast?.current?.time, "T")}</p>
+        <div className="dark:bg-zinc-600/40 rounded-lg p-[6px_12px_8px] flex flex-col gap-[16px]">
+            <p className="font-light">Прогноз на {formatTime(forecast?.current?.time, "T")}</p>
 
             <div className="flex items-center gap-[10px]">
                 <div className="size-[5rem]">
@@ -21,16 +19,16 @@ export const TodayForecastCard = () => {
                 <div className="flex gap-[16px]">
                     <p className="font-medium text-6xl">{Math.round(forecast?.current?.temperature_2m)}&deg;</p>
                     <div className="flex flex-col justify-between pt-[0.6rem] gap-[4px]">
-                        <p className="text-sm">Ощущается
+                        <p className="text-sm font-light">Ощущается
                             как {Math.round(forecast?.current?.apparent_temperature)}&deg;</p>
-                        <p className="text-sm">Ночью {Math.round(forecast?.daily?.temperature_2m_min?.[0])}&deg;</p>
+                        <p className="text-sm font-light">Ночью {Math.round(forecast?.daily?.temperature_2m_min?.[0])}&deg;</p>
                     </div>
                 </div>
             </div>
 
             <div className="flex gap-[30px]">
                 <div className="flex flex-col items-center gap-[4px]">
-                    <p>Ветер</p>
+                    <p className="dark:text-blue-200 font-light">Ветер</p>
                     <p className="flex items-center gap-[4px] font-semibold">
                         <MoveUp
                             size="0.875rem"
@@ -41,17 +39,17 @@ export const TodayForecastCard = () => {
                 </div>
 
                 <div className="flex flex-col items-center gap-[4px]">
-                    <p>Влажность</p>
+                    <p className="dark:text-blue-200 font-light">Влажность</p>
                     <p className="font-semibold">{forecast?.current?.relative_humidity_2m}%</p>
                 </div>
 
                 <div className="flex flex-col items-center gap-[4px]">
-                    <p>Давление</p>
+                    <p className="dark:text-blue-200 font-light">Давление</p>
                     <p className="font-semibold">{Math.round(forecast?.current?.surface_pressure)} мбар</p>
                 </div>
 
                 <div className="flex flex-col items-center gap-[4px]">
-                    <p>УФ-индекс</p>
+                    <p className="dark:text-blue-200 font-light">УФ-индекс</p>
                     <p className="font-semibold">{forecast?.daily?.uv_index_max?.[0]}</p>
                 </div>
             </div>
