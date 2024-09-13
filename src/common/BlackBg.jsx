8c6f44a5d351291d5b089ea05aab3ce19c9b40b7
mmorrii <byryshka55@gmail.com>
2024-09-13 22:15:15 +0300

    delete old code and assets

--- src/common/BlackBg.jsx
+++
@@ -1,9 +0,0 @@
-const BlackBg = ({ children, className }) => {
-	return (
-		<div className={`bg-black rounded-2xl px-3 bg-opacity-30 pb-1 ${className}`}>
-			{children}
-		</div>
-	)
-}
-
-export default BlackBg
--- src/common/Caption.jsx
+++
@@ -1,7 +0,0 @@
-export const Figcaption = ({ children }) => {
-	return (
-		<figcaption className="text-center text-sm opacity-70 mt-2">
-			{ children }
-		</figcaption>
-	)
-}
