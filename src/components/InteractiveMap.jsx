5512c5d27b58582e68fd2ff170ce31636d1e28ca
mmorrii <byryshka55@gmail.com>
2024-09-22 15:35:20 +0300

    fix: fixed camera positioning when switching between routes

--- src/components/InteractiveMap.jsx
+++ src/components/InteractiveMap.jsx
@@ -1,7 +1,7 @@
 import Map, {FullscreenControl, Marker, NavigationControl, ScaleControl} from 'react-map-gl/maplibre';
 import 'maplibre-gl/dist/maplibre-gl.css';
 import {useLocality} from "../hooks/useLocality.js";
-import {memo, Suspense, useEffect, useMemo, useRef, useState} from "react";
+import {useEffect, useMemo, useRef, useState} from "react";
 import { Check, X } from 'lucide-react';
 import {useDarkMode} from "../hooks/useDarkMode.js";
 
@@ -11,23 +11,16 @@ const getMapStyle = (isDark) => {
         : `https://api.maptiler.com/maps/ch-swisstopo-lbm-grey/style.json?key=${import.meta.env.VITE_MAP_STYLE_KEY}`
 }
 
-export const InteractiveMap = memo(function InteractiveMap() {
+/** Interactive map with the ability to change current location */
+export const InteractiveMap = () => {
     const {location, setLocation} = useLocality()
     const mapRef = useRef()
-
-    console.log(mapRef)
 
     const [coords, setCoords] = useState(null)
     const [action, setAction] = useState(false)
 
     const {darkMode} = useDarkMode()
     const mapStyle = useMemo(() => getMapStyle(darkMode), [darkMode])
-
-    useEffect(() => {
-        if (location.latitude && location.longitude) {
-            mapRef.current?.flyTo({center: [location.longitude, location.latitude]})
-        }
-    }, [location]);
 
     useEffect(() => {
         if (action) {
@@ -42,45 +35,37 @@ export const InteractiveMap = memo(function InteractiveMap() {
 
             return () => setAction(false)
         }
-    }, [action]);
-
-    const handleClick = (event) => {
-        const { lngLat } = event
-        setCoords(lngLat)
-    }
+    }, [action])
 
     return (
-        <Suspense fallback={<div>Loading...</div>}>
-            <Map
-                ref={mapRef}
-                initialViewState={{
-                    longitude: location?.longitude,
-                    latitude: location?.latitude,
-                    zoom: 8
-                }}
-                dragRotate={true} style={{width: "100%", height: "100%"}}
-                mapStyle={mapStyle}
-                onContextMenu={handleClick}
-            >
-                <FullscreenControl/>
-                <Marker longitude={location?.longitude ?? location?.lon} latitude={location?.latitude ?? location?.lat} color={"#dc2626"} />
-                {coords && <Marker longitude={coords?.lng} latitude={coords?.lat} draggable={true} color={"#16a34a"}
-                                   onDrag={({lngLat}) => setCoords(lngLat)} />
-                }
-                <NavigationControl position="top-left"/>
-                <ScaleControl position="bottom-left"/>
-                {coords && <Modal coords={coords} setCoords={setCoords} setAction={setAction} mapRef={mapRef.current} />}
-            </Map>
-        </Suspense>
+        <Map
+            ref={mapRef}
+            initialViewState={{
+                longitude: location?.longitude ?? location?.lon,
+                latitude: location?.latitude ?? location?.lat,
+                zoom: 8
+            }}
+            dragRotate={true} style={{width: "100%", height: "100%"}}
+            mapStyle={mapStyle}
+            onContextMenu={(e) => setCoords(e.lngLat)}
+        >
+            <FullscreenControl/>
+            <Marker longitude={location?.longitude ?? location?.lon} latitude={location?.latitude ?? location?.lat} color={"#dc2626"} />
+            {coords && <Marker longitude={coords?.lng} latitude={coords?.lat} draggable={true} color={"#16a34a"}
+                               onDrag={({lngLat}) => setCoords(lngLat)} />
+            }
+            <NavigationControl position="top-left"/>
+            <ScaleControl position="bottom-left"/>
+            {coords && <Modal coords={coords} setCoords={setCoords} setAction={setAction} mapRef={mapRef.current} />}
+        </Map>
 
     )
-})
+}
 
 const Modal = ({ coords, setCoords, setAction, mapRef }) => {
     return (
-        <div
-            className="bg-neutral-800 text-[13px] rounded-full p-[3px_4px_4px_10px] absolute top-[10px] left-1/2 -translate-x-1/2 flex items-center gap-[6px]">
-            <p className="flex items-center fontSize13">Изменить местоположение на -
+        <div className="bg-neutral-800 fontSize13 rounded-full p-[3px_4px_4px_10px] absolute top-[10px] left-1/2 -translate-x-1/2 flex items-center gap-[6px]">
+            <p className="flex items-center truncate">Изменить местоположение на -
                 <span className="ml-[4px] max-w-24 truncate"><b className="mr-[2px] dark:text-blue-500">Ш:</b>{coords.lat}</span>,
                 <span className="ml-[4px] max-w-24 truncate"><b className="mr-[2px] dark:text-green-500">Д:</b>{coords.lng}</span>
             </p>
