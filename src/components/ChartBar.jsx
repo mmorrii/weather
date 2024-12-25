9d3cca57ffebda3632d7dc17df3daa7bc9e7114f
mmorrii <byryshka55@gmail.com>
2024-12-25 20:37:28 +0200

    added highlighting of the current value to ChartBar.jsx and shortened description of functions

--- src/components/ChartBar.jsx
+++ src/components/ChartBar.jsx
@@ -1,44 +1,54 @@
 import { useForecast } from "../hooks/useForecast.js"
 import { useEffect, useState } from "react"
 import { chartDataGeneration } from "../utils/chartDataUtils.js"
-import { BarChart, Bar, ResponsiveContainer, XAxis, YAxis, CartesianGrid } from "recharts"
+import { BarChart, Bar, ResponsiveContainer, XAxis, YAxis, CartesianGrid, ReferenceArea } from "recharts"
 import { useHorizontalScroll } from "../hooks/useHorizontalScroll.js"
 
 export const ChartBar = ({ openCard = 0, timeTrack = true }) => {
-    const { forecast } = useForecast()
-    const scrollRef = useHorizontalScroll()
+   const { forecast } = useForecast()
+   const scrollRef = useHorizontalScroll()
 
-    const [data, setData] = useState([])
+   const [data, setData] = useState([])
+   const [currentIndex, setCurrentIndex] = useState(null)
 
-    useEffect(() => {
-        const result = chartDataGeneration(forecast?.hourly?.time, forecast?.hourly?.precipitation_probability, openCard, { timeTrack: timeTrack })
-        setData(result)
-    }, [forecast, openCard])
+   useEffect(() => {
+      const {arr, currentIndex} = chartDataGeneration(forecast?.hourly?.time, forecast?.hourly?.precipitation_probability, openCard, { timeTrack: timeTrack })
+      setData(arr)
+      setCurrentIndex(currentIndex)
+   }, [forecast, openCard])
 
-    return (
-        <div className="w-full h-full overflow-x-auto scrollbar" ref={scrollRef}>
-            <div className="w-[820px] h-full">
-                <ResponsiveContainer height="100%" width="100%">
-                    <BarChart
-                        data={data} barSize={8}
-                        margin={{ top: 15, right: 0, left: 0, bottom: 6 }}
-                    >
-                        <CartesianGrid strokeDasharray="5" vertical={false} stroke={"#E4E4E735"} />
-                        <XAxis dataKey="x" height={20} axisLine={false}
-                               tick={{ fontSize: 13, fontWeight: 400, fill: "#E4E4E759" }} />
-                        <YAxis domain={[0, 100]} hide={true} />
-                        <Bar
-                            dataKey="y"
-                            fill={"#bfdbfe"}
-                            animationEasing={"ease-in-out"}
-                            animationDuration={200}
-                            radius={[10, 10, 0, 0]}
-                            label={{ position: "top", fontSize: 12, fill: "#E4E4E7", formatter: (v) => v + "%" }}
-                        />
-                    </BarChart>
-                </ResponsiveContainer>
-            </div>
-        </div>
-
-    )
+   return (
+      <div className="w-full h-full overflow-x-auto scrollbar" ref={scrollRef}>
+         <div className="w-[820px] h-full">
+            <ResponsiveContainer height="100%" width="100%">
+               <BarChart
+                  data={data} barSize={8}
+                  margin={{ top: 15, right: 0, left: 0, bottom: 6 }}
+               >
+                  <CartesianGrid strokeDasharray="5" vertical={false} className="dark:stroke-zinc-200/20 stroke-black/20" />
+                  <XAxis dataKey="x" height={20} axisLine={false}
+                         tick={{ fontSize: 13, fontWeight: 400, fill: "#E4E4E759" }} />
+                  <YAxis domain={[0, 100]} hide={true} />
+                  {/* Translucent background to highlight the current value */}
+                  {currentIndex && (
+                     <ReferenceArea
+                        x1={data[currentIndex].x}
+                        x2={data[currentIndex].x}
+                        className="dark:fill-zinc-600 dark:opacity-20"
+                        stroke="none"
+                     />
+                  )}
+                  <Bar
+                     dataKey="y"
+                     fill={"#bfdbfe"}
+                     animationEasing={"ease-in-out"}
+                     animationDuration={200}
+                     radius={[10, 10, 0, 0]}
+                     label={{ position: "top", fontSize: 12, fill: "#E4E4E7", formatter: (v) => v + "%" }}
+                  />
+               </BarChart>
+            </ResponsiveContainer>
+         </div>
+      </div>
+   )
 }
--- src/utils/chartDataUtils.js
+++ src/utils/chartDataUtils.js
@@ -2,18 +2,14 @@ import { formatTime } from "./formatTime.js"
 import { DateTime } from "luxon"
 
 /**
- * Returns a slice of the array based on the pagination index, with an option to track time for the first page.
- *
- * The function paginates the input array into chunks of 24 items. If the `index` is 0 and `timeTrack` option is enabled,
- * the function will determine the starting index based on the current hour of the day. Otherwise, it will use the provided
- * pagination index to compute the start and end indices for slicing the array.
+ * Function splitting an array into 24 elements with the ability to track time on the first page
  *
  * @param {Array} array - The array of items to paginate.
  * @param {number} index - The current pagination index.
  * @param {Object} [opt] - Optional configuration object.
- * @param {boolean} [opt.timeTrack=true] - Optional parameter that determines whether indexing should start from the current time
+ * @param {boolean} [opt.timeTrack=true] - Optional parameter determining whether the array should start from the current time
  *
- * @returns {Array} - A slice of the array, containing up to 24 items starting from the calculated index.
+ * @returns {Array} - The array with 24 items
  */
 const getPaginatedItems = (array, index, opt = { timeTrack: true }) => {
     let startIndex, endIndex
@@ -30,32 +26,32 @@ const getPaginatedItems = (array, index, opt = { timeTrack: true }) => {
 }
 
 /**
- * Generates chart data by paginating the input `x` and `y` arrays and formatting them into an array of objects.
+ * Function for generating an array of objects for a chart with x-axis (time) and y-axis (values) and current index
  *
- * The function paginates the `x` and `y` data arrays, formats the `x` data using the specified time format, and
- * pairs each `x` value with its corresponding `y` value. The result is an array of objects where each object
- * contains an `x` (formatted time) and `y` value.
- *
- * Used by {@link ../components/ChartBar.jsx ChartBar.jsx}
- *
- * @param {Array} x - The array representing the x-axis data (typically timestamps or time-related data).
- * @param {Array} y - The array representing the y-axis data.
- * @param {number} index - The current pagination index.
- * @param {Object} [opt] - Optional configuration object passed to control pagination behavior (e.g., time-based tracking).
+ * @param {Array} x - The array timestamps
+ * @param {Array} y - The array values
+ * @param {number} index - The current pagination index
+ * @param {Object} [opt] - Optional configuration
  * @param {boolean} [opt.timeTrack] - Optional parameter that determines whether indexing should start from the current time
  *
- * @returns {Array<{x: string, y: any}>} - An array of objects, each containing formatted `x` values and their corresponding `y` values.
+ * @returns {{arr: Array<{x: string, y: any}>, currentIndex: number | null}}
  */
 export const chartDataGeneration = (x, y, index, opt) => {
     const visibleXData = getPaginatedItems(x, index, opt)
     const visibleYData = getPaginatedItems(y, index, opt)
 
     const arr = []
+    let currentIndex = null
 
     for (let i = 0; i < 24; i++) {
         const formatData = formatTime(visibleXData[i], "HH")
         arr.push({ x: formatData, y: visibleYData[i] })
     }
 
-    return arr
+    if (index === 0 && !opt.timeTrack) {
+        const currentTime = DateTime.local().toFormat("HH")
+        currentIndex = arr.findIndex((item) => item.x === currentTime)
+    }
+
+    return { arr: arr,  currentIndex: currentIndex }
 }
